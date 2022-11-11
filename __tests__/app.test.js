6250487b1eb1c77dbd2c16e3327bfd5228f78a8e
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-10 17:19:32 -0800

    add test and table for cats

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/cats should return a list of cat breeds', async () => {
+    const res = await request(app).get('/cats');
+    const expected = {
+      name: expect.any(String),
+      playfulness: expect.any(Number),
+      intelligence: expect.any(Number),
+      health: expect.any(Number),
+      shedding: expect.any(Number),
+      pet_friendly: expect.any(Number),
+    }
+    expect(res.body).toEqual(expected);
   });
+  
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS cats_table;
+
+CREATE TABLE cats_table (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    playfulness INT NOT NULL,
+    intelligence INT NOT NULL,
+    health INT NOT NULL,
+    shedding INT NOT NULL,
+    pet_friendly INT NOT NULL
+);
+
+INSERT INTO cats_table (name, playfulness, intelligence, health, shedding, pet_friendly)
+VALUES
+('Abyssinian', 5, 5, 2, 3, 5),
+('Russian Blue', 2, 4, 4, 4, 4),
+('Scottish Fold', 3, 4, 3, 3, 4),
+('British Shorthair', 2, 3, 4, 3, 5),
+('Ragdoll', 4, 4, 3, 4, 4),
+('Sphynx', 4, 4, 2, 1, 5),
+('Burmese', 5, 5, 1, 2, 5),
+('Maine Coon', 4, 4, 3, 4, 5);
